1a87af8e5b87cea472b7c074b072a5e14da7865e
Conrad Weiser <vibrantclouds@protonmail.com>
2018-09-14 23:03:46 -0400

    Some things didn't make it into last commit

--- Server/models/DatabaseConnector.ts
+++ Server/models/DatabaseConnector.ts
@@ -8,10 +8,15 @@ export default class DatabaseConnector {
         mongoose.connect('mongodb://testuser:testuser1@ds155862.mlab.com:55862/tradeoff', {useNewUrlParser: true})
         let db = mongoose.connection;
 
-        // Give notification as to if our connection succeeded or not
+        // Give a console notification as to if our connection succeeded or not (async)
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', function() {
             console.log('Connected to the mongoDB cloud server!');
         });
+
+        /*
+        * Create your Mongoose Schema here? Or make this abstract, and extend it for more Object-Oriented type design!
+        * You decide!
+         */
     }
 }
