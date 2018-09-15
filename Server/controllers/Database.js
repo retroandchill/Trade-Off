4be207e3f1ac5f6397ff14fd9b3aa330af2992f4
Conrad Weiser <vibrantclouds@protonmail.com>
2018-09-14 22:00:46 -0400

    Untracked JS files -- Regenerated TS config

--- Server/controllers/Database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Import database class
-Object.defineProperty(exports, "__esModule", { value: true });
-var DatabaseConstructor = /** @class */ (function () {
-    function DatabaseConstructor() {
-    }
-    return DatabaseConstructor;
-}());
-exports.default = DatabaseConstructor;
--- Server/server.ts
+++ Server/server.ts
@@ -1,4 +1,3 @@
-
 // Import the environmental variables from the configuration file
 require ('dotenv').config();
 import app from './models/app';
@@ -7,5 +6,5 @@ const port: number = Number(process.env.PORT) || 3000;
 app.set('port', port);
 
 app.listen(app.get('port'), () => {
-    console.log('Listening at localhost:${port}/');
+    console.log('Listening at localhost:' + port + "/");
 });
