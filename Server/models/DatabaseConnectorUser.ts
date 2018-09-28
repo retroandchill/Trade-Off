c4fbd324f5d1d133258a568d6addbad63f173f27
Jordon Torunian <jordontorunian@gmail.com>
2018-09-27 21:51:27 -0400

    Login and Register (and bugs)

--- Server/models/DatabaseConnectorUser.ts
+++ Server/models/DatabaseConnectorUser.ts
@@ -1,5 +1,6 @@
 import DatabaseConnector from "./DatabaseConnector";
-import {Schema} from "mongoose";
+import {default as mongoose, Schema} from "mongoose";
+import bcrypt from 'bcrypt';
 
 export default class DatabaseConnectorUser extends DatabaseConnector {
 
@@ -16,5 +17,36 @@ export default class DatabaseConnectorUser extends DatabaseConnector {
 
     }
 
+    public loginUser(userName, passHash, passSalt){
+        var passUserDataModel = mongoose.model('passUserDataModel', this.user);
+        var passUserDataModel = passUserDataModel.findOne({'userName': 'userName'});
+        passUserDataModel.select('userName');
+
+        var passUserPassModel = mongoose.model('passUserPassModel', this.user);
+        var passUserPassModel = passUserDataModel.findOne({'passHash': 'passHash'});
+        passUserPassModel.select('passHash');
+
+        passUserDataModel.exec(function (err, userName) {
+            if (err) return handleError(err); //idk what this is dan just copy pasted it u judge
+            console.log('Wrong user name or password: %s', userName);
+        }
+
+        return (bcrypt.compareSync(myPlaintextPassword, passHash)); //myplaintextpassword should be the plaintextpassword we're fuckin something up
+        // fuk u bcrypt
+
+
+    }
+
+    public registerUser(userName, passWord, email){
+        var salt = bcrypt.genSaltSync(saltRounds); //this value needs to exist probably hard code it lol
+        var hash = bcrypt.hashSync(passWord, salt);
+
+        var user = new user({ username: userName, passHash: hash, email: email, passSalt: salt });
+        user.save(function (err) {
+            if (err) return handleError(err); // what is this errorhandler shit
+            // saved!
+        });
+
+    }
 
 }
