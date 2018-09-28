1449cb39ffd409071140f0a33c5d69ec9a86fbe9
fcorso2016 <fcorso2016@gmail.com>
2018-09-27 22:04:27 -0400

    Created signup form and added functionality to the buttons

--- Client/views/js/LoginController.js
+++ Client/views/js/LoginController.js
@@ -1,5 +1,13 @@
 var $ = require('jQuery');
 
-function displayLogin() {
+function displaySignup() {
     $("#register").modal('show');
+}
+
+function processLogin() {
+    // STUB: Will ping the server once I get more info from Conrad
+}
+
+function processSignup() {
+    // STUB: Will ping the server once I get more info from Conrad
 }
