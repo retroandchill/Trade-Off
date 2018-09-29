f928ae3232f46baeeea2f325fa920f03d9a114f8
Cole Trexler <cwtrexler@ymail.com>
2018-09-29 17:14:57 -0400

    AJAX

--- Client/views/js/LoginController.js
+++ Client/views/js/LoginController.js
@@ -6,8 +6,74 @@ function displaySignup() {
 
 function processLogin() {
     // STUB: Will ping the server once I get more info from Conrad
+    let postData = $("#loginForm").serializeArray();
+
+    console.log(postData);
+
+    let loginData = {'Email': postData.email, 'Password': postData.password};
+
+    $.ajax({
+        type: 'POST',
+        data: loginData,
+        url: 'http://localhost:3000/loginhandler',
+
+        error: (xhr, status, error) => {
+            console.log(error);
+        }
+    });
 }
 
 function processSignup() {
     // STUB: Will ping the server once I get more info from Conrad
+    let postData = $("#registerForm").serializeArray();
+
+    console.log(postData);
+
+
+    let loginData = {'Username': postData.username, 'Email': postData.email, 'Password': postData.password};
+
+
+    $.ajax({
+        type: 'POST',
+        data: loginData,
+        url: 'http://localhost:3000/registrationhandler',
+
+        error: (xhr, status, error) => {
+            console.log(error);
+        }
+    });
 }
+
+/*
+$('#registerForm')
+    .form({
+        fields: {
+            email: {
+                identifier  : 'email',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter your e-mail'
+                    },
+                    {
+                        type   : 'email',
+                        prompt : 'Please enter a valid e-mail'
+                    }
+                ]
+            },
+            password: {
+                identifier  : 'password',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Please enter your password'
+                    },
+                    {
+                        type   : 'length[6]',
+                        prompt : 'Your password must be at least 6 characters'
+                    }
+                ]
+            }
+        }
+    })
+;*/
